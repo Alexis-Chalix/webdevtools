54a7c1ddb5b4d08f151843c2f13a696c958ec471
Alexis Chalande <chalande.alexis@gmail.com>
2022-11-13 03:08:59 +0100

    Configs

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mainTheme: "#1f6bff",
+      },
+      boxShadow: {
+        formInput: "0 0 0 2px #1f6bff",
+        header: "0 6px 6px rgba(31, 107, 255, 0.1);"
+      }
+    },
+  },
+  plugins: [],
+}
